c702b3d6910bc1d99a4005e25dd6fede4ba4e429
unknown <sabirchauhan171@gmail.com>
2024-09-02 12:02:50 +0530

    new update

--- Multi-Utility/Question 1.c
+++ Multi-Utility/Question 1.c
@@ -4,12 +4,12 @@
 void main() 
 {
     float celsius;
-    float f;
+    float Fahrenheit;
     
     
     printf("Enter the value of celsius: ");
     scanf("%f",&celsius) ;
-    f = celsius *9/5 + 32;
-    printf("the value of fahrenheit is:%.2f\n",f);
+    Fahrenheit= celsius *9/5 + 32;
+    printf("the value of fahrenheit is:%.2f\n",Fahrenheit);
     
 } 
