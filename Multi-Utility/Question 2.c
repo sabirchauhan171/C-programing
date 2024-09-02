9748fa102c02595f6284b7d2c42b29608e742e4d
unknown <sabirchauhan171@gmail.com>
2024-09-02 13:08:07 +0530

    new update

--- Multi-Utility/Question 2.c
+++ Multi-Utility/Question 2.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
-#include <conio.h>
+
 
 int main()
 {
-    float basesalary,hira,da,ta,gross_salary;
-    printf("enter base salary");
+    float basesalary,HRA,DA,TA,gross_salary;
+    printf("enter base salary :");
     scanf("%f",&basesalary);
 
-    printf("enter hira: ");
-    scanf("%f",&hira);
+    HRA=basesalary*10/100;
 
-    printf("enter da: ");
-    scanf("%f",&da);
+    printf("enter HRA in persentage : \n");
+    scanf("%f",&HRA);
+    DA=basesalary*5/100;
 
-    printf("enter ta: ");
-    scanf("%f",&ta);
-    gross_salary = basesalary + hira + da + ta;
+    printf("enter DA in persentage : \n");
+    scanf("%f",&DA);
+    TA=basesalary*8/100;
+
+    printf("enter TA in persentage : \n");
+    scanf("%f",&TA);
+    
+ 
+    gross_salary = basesalary + HRA + DA + TA;
     printf("gross salary: %.2f\n", gross_salary);
     return 0;
 }
